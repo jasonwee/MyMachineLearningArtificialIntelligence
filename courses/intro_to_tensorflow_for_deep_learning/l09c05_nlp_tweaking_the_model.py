d6195ae18fa3ef991af218a0a933003abf68ed6b
Jason Wee <peichieh@gmail.com>
2025-07-07 07:38:05 +0800

    model tweaking

---
+++ courses/intro_to_tensorflow_for_deep_learning/l09c05_nlp_tweaking_the_model.py
@@ -0,0 +1,141 @@
+import tensorflow as tf
+
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+
+!wget --no-check-certificate \
+    https://drive.google.com/uc?id=13ySLC_ue6Umt9RJYSeM2t-V0kCv-4C-P \
+    -O /tmp/sentiment.csv
+
+import numpy as np
+import pandas as pd
+
+dataset = pd.read_csv('/tmp/sentiment.csv')
+
+sentences = dataset['text'].tolist()
+labels = dataset['sentiment'].tolist()
+
+# Separate out the sentences and labels into training and test sets
+training_size = int(len(sentences) * 0.8)
+
+training_sentences = sentences[0:training_size]
+testing_sentences = sentences[training_size:]
+training_labels = labels[0:training_size]
+testing_labels = labels[training_size:]
+
+# Make labels into numpy arrays for use with the network later
+training_labels_final = np.array(training_labels)
+testing_labels_final = np.array(testing_labels)
+
+
+vocab_size = 500
+embedding_dim = 16
+max_length = 50
+trunc_type='post'
+padding_type='post'
+oov_tok = "<OOV>"
+
+tokenizer = Tokenizer(num_words = vocab_size, oov_token=oov_tok)
+tokenizer.fit_on_texts(training_sentences)
+word_index = tokenizer.word_index
+training_sequences = tokenizer.texts_to_sequences(training_sentences)
+training_padded = pad_sequences(training_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+testing_sequences = tokenizer.texts_to_sequences(testing_sentences)
+testing_padded = pad_sequences(testing_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+    tf.keras.layers.GlobalAveragePooling1D(),
+    tf.keras.layers.Dense(6, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+model.summary()
+
+num_epochs = 30
+history = model.fit(training_padded, training_labels_final, epochs=num_epochs, validation_data=(testing_padded, testing_labels_final))
+
+
+import matplotlib.pyplot as plt
+
+
+def plot_graphs(history, string):
+  plt.plot(history.history[string])
+  plt.plot(history.history['val_'+string])
+  plt.xlabel("Epochs")
+  plt.ylabel(string)
+  plt.legend([string, 'val_'+string])
+  plt.show()
+  
+plot_graphs(history, "accuracy")
+plot_graphs(history, "loss")
+
+
+# First get the weights of the embedding layer
+e = model.layers[0]
+weights = e.get_weights()[0]
+print(weights.shape) # shape: (vocab_size, embedding_dim)
+
+
+import io
+
+# Create the reverse word index
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+
+# Write out the embedding vectors and metadata
+out_v = io.open('vecs.tsv', 'w', encoding='utf-8')
+out_m = io.open('meta.tsv', 'w', encoding='utf-8')
+for word_num in range(1, vocab_size):
+  word = reverse_word_index[word_num]
+  embeddings = weights[word_num]
+  out_m.write(word + "\n")
+  out_v.write('\t'.join([str(x) for x in embeddings]) + "\n")
+out_v.close()
+out_m.close()
+
+
+# Download the files
+try:
+  from google.colab import files
+except ImportError:
+  pass
+else:
+  files.download('vecs.tsv')
+  files.download('meta.tsv')
+
+
+# Use the model to predict a review   
+fake_reviews = ['I love this phone', 'I hate spaghetti', 
+                'Everything was cold',
+                'Everything was hot exactly as I wanted', 
+                'Everything was green', 
+                'the host seated us immediately',
+                'they gave us free chocolate cake', 
+                'not sure about the wilted flowers on the table',
+                'only works when I stand on tippy toes', 
+                'does not work when I stand on my head']
+
+print(fake_reviews) 
+
+# Create the sequences
+padding_type='post'
+sample_sequences = tokenizer.texts_to_sequences(fake_reviews)
+fakes_padded = pad_sequences(sample_sequences, padding=padding_type, maxlen=max_length)           
+
+print('\nHOT OFF THE PRESS! HERE ARE SOME NEWLY MINTED, ABSOLUTELY GENUINE REVIEWS!\n')              
+
+classes = model.predict(fakes_padded)
+
+# The closer the class is to 1, the more positive the review is deemed to be
+for x in range(len(fake_reviews)):
+  print(fake_reviews[x])
+  print(classes[x])
+  print('\n')
+
+# Try adding reviews of your own
+# Add some negative words (such as "not") to the good reviews and see what happens
+# For example:
+# they gave us free chocolate cake and did not charge us
